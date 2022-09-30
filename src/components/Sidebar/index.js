7eae0c90606a92b07e2ca902934df0dcfaa8322f
Ibrahim Alassaf <xibra9141@gmail.com>
2022-09-30 19:52:01 +0300

    feat: create src/components/Sidebar/index.js

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { CloseIcon, Icon, SidebarContainer } from "./SidebarElements";
+export default function Sidebar() {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+}
