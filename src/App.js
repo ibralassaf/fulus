e0f0cca76b9fc97cd3ed8bf93008b2e430489f37
Ibrahim Alassaf <xibra9141@gmail.com>
2022-09-30 19:51:42 +0300

    feat: update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
