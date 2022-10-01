637313ddcdee73439e8b8f5a815470818f640d2d
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-01 20:09:53 +0300

    fear: add new data.js

--- src/components/InfoSection/data.js
+++ src/components/InfoSection/data.js
@@ -1,4 +1,5 @@
 export const homeObjOne = {
+  id: "about",
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
@@ -13,4 +14,40 @@ export const homeObjOne = {
   dark: true,
   primary: true,
   darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account at any time",
+  description:
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.",
+  buttonLabel: "Learn More",
+  imgStart: "true",
+  img: require("../../images/svg-2.svg").default,
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: "false",
+  img: require("../../images/svg-6.svg").default,
+  alt: "Guy holding card",
+  dark: false,
+  primary: false,
+  darkText: true,
 };
