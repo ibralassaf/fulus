dfb4e42612365ea8f6b4e12ccc1e6139be56cc0d
Ibrahim Alassaf <xibra9141@gmail.com>
2022-10-01 03:18:59 +0300

    feat: create data.js

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: "",
+  img: require("../../images/svg-1.svg").default,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
